7ce1deef6b2884028eeb5c1702b819c60bdc72b2
Aishwarya Ambule <aishwaryaambule@gmail.com>
2024-09-13 20:35:42 +0530

    ff

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css'
 import logo from '../../assets/logo.png';
 import arrow_icon from '../../assets/arrow_icon.png';
 import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -22,7 +23,7 @@ const Navbar = () => {
         setCurrency({ name: "inr", symbol: "₹" })
         break;
       }
-      default : {
+      default: {
         setCurrency({ name: "usd", symbol: "$" })
         break;
       }
@@ -31,9 +32,11 @@ const Navbar = () => {
 
   return (
     <div className='navbar' >
-      <img src={logo} alt="" className='logo' />
+      <Link to={'/'}>
+        <img src={logo} alt="" className='logo' />
+      </Link>
       <ul>
-        <li>Home</li>
+        <Link to={'/'}><li>Home</li></Link>
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
--- src/components/pages/Coin/Coin.jsx
+++ src/components/pages/Coin/Coin.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Coin.css'
+import { useParams } from 'react-router-dom'
 
 const Coin = () => {
+
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+
+  const fetchCoinData = async() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'x-cg-demo-api-key': '	CG-QYMi4PH4iDwFvukoRRd9eDDq'
+      }
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then(response => response.json())
+      .then(response => setCoinData(response))
+      .catch(err => console.error(err));
+  }
+
+  useEffect(()=>{
+    fetchCoinData();
+  },[])
+
   return (
-    <div>Coin</div>
+    <div>
+      <h2>Coin : {coinId}</h2>
+    </div>
   )
 }
 
-export default Coin
+export default Coin;
